4d10f50da468a152ec01361cf2e3ce30ebf548ea
rich-p-rich <>
2024-01-30 21:57:09 +0000

    Link index.html to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("how-to-play").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("game-rules").classList.toggle("show");
+}
+
+
+function myFunction() {
+    document.getElementById("how-to-play").classList.toggle("show");
+}
