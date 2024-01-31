c8dd478d1fd9f2ad0eaf3906f47686ee133a0cb9
Rich Ricot <richricot@gmail.com>
2024-01-31 22:09:48 +0000

    Add math.floor to 'Target Number' code & increase range of possible numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,6 @@ function readyFizzBuzz() {
 // generate the sequence of Target numbers //
 document.getElementById("set-target-numbers").addEventListener("click", setTargetNumbers);
 function setTargetNumbers () {
-    TargetNumbers = Math.random () * 10; 
+    TargetNumbers = Math.floor(Math.random()*100) + 3; 
     document.getElementById("target-number").innerHTML = TargetNumbers; 
 }
