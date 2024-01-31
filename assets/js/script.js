b13a48adbb2c11ca13cd2857d4459ef3e191be49
rich-p-rich <>
2024-01-31 13:22:48 +0000

    Fix link from HTML to JS, implement toggle 'show/hide' button for game rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 document.getElementById("how-to-play").addEventListener("click", myFunction);
 
 function myFunction() {
-    document.getElementById("game-rules").classList.toggle("show");
+    document.getElementById("game-rules").classList.toggle("hide");
 }
 
 let randomNum = Math.floor((Math.random() * 10) + 1);
