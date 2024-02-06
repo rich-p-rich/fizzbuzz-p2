51fbac00591635b4f9e59c73e269d71780ab7d22
Rich Ricot <richricot@gmail.com>
2024-02-06 17:09:17 +0000

    Continue debug display answer section, add CSS styles to buttons for rules, set FB and Target Numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,58 @@
-// Set up order of operations (load DOM then game); add event listeners to the buttons //
+// Set up order of operations (load DOM then game); add event listeners to the buttons
 document.addEventListener("DOMContentLoaded", function () {
   let answerButtons = document.querySelectorAll(".answer-buttons");
 })
 
-// show / hide the 'How the games works' section //
+// toggle between 'show' and 'hide' for the 'How the games works' section
 document.getElementById("how-to-play").addEventListener("click", showHideGameplay);
 function showHideGameplay() {
   document.getElementById("game-rules").classList.toggle("hide");
 }
 
-// conditions to ensure the FizzBuzz numbers are between 2 and 9, and cannot be the same //
-document.getElementById("ready-fizzbuzz").addEventListener("click", readyFizzBuzz);
-document.getElementById("ready-fizzbuzz").min = "2", max = "9";
+// conditions to ensure the FizzBuzz numbers are between 2 and 9
+parseInt(document.getElementById("ready-fizzbuzz").addEventListener("click", readyFizzBuzz));
+parseInt(document.getElementById("ready-fizzbuzz").min = "2", max = "9");
 
-// generate the Fizz and Buzz numbers //
-// ensure the Fizz and Buzz numbers are not the same//
+// generate the Fizz and Buzz numbers and ensure they are not identical
 function readyFizzBuzz() {
   FizzNumber = Math.floor(Math.random() * 8) + 2;
   document.getElementById("fizz-number").innerText = FizzNumber;
-   do { 
+  do {
     BuzzNumber = Math.floor(Math.random() * 8) + 2;
-  } while (BuzzNumber === FizzNumber) {
-    document.getElementById("buzz-number").innerText = BuzzNumber;
-}
+  } while (BuzzNumber === FizzNumber);
+  document.getElementById("buzz-number").innerText = BuzzNumber;
 }
 
-// generate the sequence of Target numbers //
-document.getElementById("set-target-numbers").addEventListener("click", setTargetNumbers);
-document.getElementById("target-number").min = "10", max = "99";
+// generate the sequence of Target numbers
+parseInt(document.getElementById("set-target-numbers").addEventListener("click", setTargetNumbers));
 function setTargetNumbers() {
-  clearResult();
-  TargetNumbers = Math.floor(Math.random() * 100) + 1;
-  document.getElementById("target-number").innerText = TargetNumbers;
+  {
+    clearResult();
+    TargetNumbers = Math.floor(Math.random() * 100) + 1;
+    parseInt(document.getElementById("target-number").innerText = TargetNumbers).TargetNumbers;
+  } while (TargetNumbers < 10) {
+    TargetNumbers = Math.floor(Math.random() * 100) + 1;
+    parseInt(document.getElementById("target-number").innerText = TargetNumbers);
+  }
 }
 
-// clear the answer field with 'new target number' //
+// clear the answer field with 'new target number'
 function clearResult() {
   document.getElementById("display-result").innerText = "";
 };
 
-//check what the right answer should be// 
-function calculateCorrectAnswer() {
+// calculate the correct answer
+function correctAnswer() {
   let targetNumber = parseInt(document.getElementById("target-number").innerText);
   let fizzNumber = parseInt(document.getElementById("fizz-number").innerText);
   let buzzNumber = parseInt(document.getElementById("buzz-number").innerText);
 
   let correctAnswer = "";
-  if (targetNumber % fizzNumber == 0) {
+  if (targetNumber % fizzNumber === 0) {
     correctAnswer = "Fizz";
   } else if (targetNumber % buzzNumber === 0) {
     correctAnswer = "Buzz";
-  } else if (targetNumber % fizzNumber !=== 0 && targetNumber % buzzNumber !== 0) {
+  } else if (targetNumber % fizzNumber !== 0 && targetNumber % buzzNumber !== 0) {
     correctAnswer = "No!";
   } else if (targetNumber % fizzNumber === 0 && targetNumber % buzzNumber === 0) {
     correctAnswer = "FizzBuzz";
@@ -58,56 +60,46 @@ function calculateCorrectAnswer() {
   return correctAnswer;
 }
 
-//FizzNumber = Math.floor(Math.random() * 8) + 2;//
-//document.getElementById("fizz-number").innerHTML = FizzNumber;//
-
-// Display the result in the element with id "result" //
+// Display the result based on user's answer
 function displayResult() {
-  document.getElementById("display-result").innerText = result();
+  let userAnswer = getUserAnswer();
+  let correct = correctAnswer();
+  let output = "";
+  if (correct === userAnswer) {
+    output = "Correct!";
+    scoreCorrect();
+  } else {
+    output = "Incorrect!";
+    scoreIncorrect();
+  }
+  document.getElementById("display-result").innerText = output;
 }
 
-// Check user answer against calculatedCorrectAnswer //
-let answer = calculateCorrectAnswer();
-let userAnswer = userInputAnswer();
-
-let result =
-  function () {
-    let output = "";
-    if (answer === userAnswer) {
-      output = "Correct!"
-    } else {
-      output = "Incorrect!"
-    }
-    return output;
-  };
-
-//read answer from user's button click//
-function userInputAnswer() {
-  document.getElementById("fizz-btn").addEventListener("click", function () {
-    userInputAnswer("Fizz");
-    displayResult();
-  });
-
-  document.getElementById("buzz-btn").addEventListener("click", function () {
-    userInputAnswer("Buzz");
-    displayResult();
-  });
-
-  document.getElementById("no-btn").addEventListener("click", function () {
-    userInputAnswer("No!");
-    displayResult();
-  });
-
-  document.getElementById("fizzbuzz-btn").addEventListener("click", function () {
-    userInputAnswer("FizzBuzz");
-    displayResult();
-  });
+// Identify what the user answer is
+function getUserAnswer() {
+  if (document.getElementById("fizz-btn").checked) {
+    return Fizz;
+  } else if (document.getElementById("buzz-btn").checked) {
+    return Buzz;
+  } else if (document.getElementById("no-btn").checked) {
+    return No;
+  } else if (document.getElementById("fizzbuzz-btn").checked) {
+    return FizzBuzz;
+  } else {
+    return "";
+  }
 }
-
-// Get new target number to continue the game //
-document.getElementById("next-target-number").addEventListener("click", setTargetNumbers);
+// Get new target number to continue the game
+parseInt(document.getElementById("next-target-number").addEventListener("click", setTargetNumbers));
 document.getElementById("next-target-number").min = "10", max = "99";
-/* function setNewTargetNumber () {
-   TargetNumbers = Math.floor(Math.random()*100) + 3; 
-   document.getElementById("next-target-number").innerHTML = TargetNumbers;
-}*/
+
+// keep score - correct answers
+function scoreCorrect() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("correct-answers").innerText = ++oldScore;
+}
+
+function scoreIncorrect() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("incorrect-answers").innerText = ++oldScore;
+}
